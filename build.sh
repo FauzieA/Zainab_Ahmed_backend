749bca4602b9e8eb34ae2c461ec77d93d4704f4b
FauzieA <fauxieabdullahi22@gmail.com>
2026-06-07 18:14:40 +0800

    auto superuser

--- build.sh
+++ build.sh
@@ -5,8 +5,12 @@ set -o errexit
 # 1. Install dependencies
 pip install -r requirements.txt
 
-# 2. Collect static files safely without asking for confirmation prompts
+# 2. Collect static files safely
 python manage.py collectstatic --no-input
 
 # 3. Run outstanding database migrations against Supabase
 python manage.py migrate
+
+# 4. AUTOMATIC SUPERUSER CREATION FOR FREE TIER
+# The "|| true" ensures future deploys don't crash when the account already exists!
+python manage.py createsuperuser --no-input || true
